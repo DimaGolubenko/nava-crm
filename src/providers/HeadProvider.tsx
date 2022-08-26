2a15214e10662de42d51f9ea380ca095056bf8e3
Dima Golubenko <dimagolubenko@gmail.com>
2022-08-26 19:28:11 +0300

    Add Head provider

---
+++ src/providers/HeadProvider.tsx
@@ -0,0 +1,8 @@
+import { ReactElement } from 'react';
+import { FC, PropsWithChildren } from 'react';
+
+export const HeadProvider: FC<PropsWithChildren> = ({
+	children,
+}): ReactElement => {
+	return <>{children}</>;
+};
